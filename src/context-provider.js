b82318c48e29a94d204050ac21adc6b3fb198837
jacobbarnes <57917206+jacobbarnes@users.noreply.github.com>
2020-04-07 20:41:44 -0400

    Update context-provider.js

--- src/context-provider.js
+++ src/context-provider.js
@@ -2,7 +2,7 @@ import React from 'react'
 // import axios from 'axios'
 import AppContext from './context'
 import App from './App'
-import campaigns from './CAMPAIGNS'
+import campaigns from './campaigns'
 
 export default class AppProvider extends React.Component {
     constructor(props) {
