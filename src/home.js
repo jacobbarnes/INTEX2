6947272790545980d1edcf95b2c4c076793a1eab
victoria-skylan <50216078+victoria-skylan@users.noreply.github.com>
2020-04-10 09:48:41 -0600

    home update

--- src/home.js
+++ src/home.js
@@ -6,9 +6,10 @@ export default function Home() {
         <div className="text-center" style={{  backgroundColor: 'rgb(250,250,250)', padding: "3%", width: "100%" }}>
             <br/>
             <hr/>
-            <h3>Here to help</h3>
+            <h3 className="text-light p-4" style={{backgroundColor: "#31b663"}}><b>Here to help</b></h3>
             <hr/>
             <h6>“If there be any truer measure of a man than by what he does, it must be by what he gives.”  ―Robert South</h6>
+            <br/>
             <img src='/GoFundMe-Logo-1.png' alt='logo' className='mainimage'/>
         </div>
     )
