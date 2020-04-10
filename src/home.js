75a098fb5f0143d8441e0cad52a03a6803edcff5
victoria-skylan <50216078+victoria-skylan@users.noreply.github.com>
2020-04-10 09:44:15 -0600

    minor home change

--- src/home.js
+++ src/home.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Col, Row} from "react-bootstrap"
 export default function Home() {
 
     return(
-        <div style={{  backgroundColor: 'rgb(250,250,250)', padding: "3%" }}>
+        <div className="text-center" style={{  backgroundColor: 'rgb(250,250,250)', padding: "3%", width: "100%" }}>
+            <br/>
+            <hr/>
+            <h3>Here to help</h3>
+            <hr/>
+            <h6>“If there be any truer measure of a man than by what he does, it must be by what he gives.”  ―Robert South</h6>
             <img src='/GoFundMe-Logo-1.png' alt='logo' className='mainimage'/>
-            <hr style={{width: "50%"}}/>
         </div>
     )
 }
